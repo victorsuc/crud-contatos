85fa541b0993312f0683a02a92228a153a4158c7
Victor Sucupira <victorsuc@hotmail.com>
2019-07-21 20:39:24 -0300

    criando projeto com spring boot

---
+++ src/test/java/br/com/appvictor/crudcontatos/CrudContatosApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.appvictor.crudcontatos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudContatosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
