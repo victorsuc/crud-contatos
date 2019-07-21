8074c037176d2702fcea73cf7c8dc710f910edd2
Victor Sucupira <victorsuc@hotmail.com>
2019-07-21 20:56:13 -0300

    Criando entidade de Telefone

---
+++ src/main/java/br/com/appvictor/crudcontatos/entity/Telefone.java
@@ -0,0 +1,29 @@
+package br.com.appvictor.crudcontatos.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Telefone {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String ddd;
+	
+	private String numero;
+}
