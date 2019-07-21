ba2072546933517c1e5a96b83d30bf754088f80b
Victor Sucupira <victorsuc@hotmail.com>
2019-07-21 20:54:45 -0300

    Criando entidade de Pessoa

---
+++ src/main/java/br/com/appvictor/crudcontatos/entity/Pessoa.java
@@ -0,0 +1,33 @@
+package br.com.appvictor.crudcontatos.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Pessoa {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String nome;
+	
+	private String cpf;
+	
+	private Date dataNascimento;
+	
+	private String email;
+}
