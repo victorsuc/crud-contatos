3f96c2fd8d067a5374aa6f2efe2defec6d2adccb
Victor Sucupira <victorsuc@hotmail.com>
2019-07-22 02:18:44 -0300

    Adicionando contatos ao levantar aplicação

--- src/main/java/br/com/appvictor/crudcontatos/CrudContatosApplication.java
+++ src/main/java/br/com/appvictor/crudcontatos/CrudContatosApplication.java
@@ -1,13 +1,29 @@
 package br.com.appvictor.crudcontatos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CrudContatosApplication {
+import br.com.appvictor.crudcontatos.entities.Pessoa;
+import br.com.appvictor.crudcontatos.repositories.PessoaRepository;
 
+@SpringBootApplication
+public class CrudContatosApplication implements CommandLineRunner{
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CrudContatosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		pessoaRepository.save(new Pessoa("victor", "1234", "qualquer"));
+		pessoaRepository.save(new Pessoa("eric", "652", "qualquer"));
+		pessoaRepository.save(new Pessoa("joao", "236", "qualquer"));
+		
 	}
 
 }
