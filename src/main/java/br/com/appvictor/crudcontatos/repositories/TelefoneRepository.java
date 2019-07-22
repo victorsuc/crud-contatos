a7b7efbcd097af6b3c1ffbca7f441e08111a6067
Victor Sucupira <victorsuc@hotmail.com>
2019-07-21 21:01:08 -0300

    Criando repositorios de pessoa e telefone para crud

---
+++ src/main/java/br/com/appvictor/crudcontatos/repositories/TelefoneRepository.java
@@ -0,0 +1,8 @@
+package br.com.appvictor.crudcontatos.repositories;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public class TelefoneRepository {
+
+}
