480df4842e3e38f3b85fcfc70ae8d5bff43adabb
Victor Sucupira <victorsuc@hotmail.com>
2019-07-21 21:34:49 -0300

    Criando repositorios das entidades

--- src/main/java/br/com/appvictor/crudcontatos/repositories/PessoaRepository.java
+++ src/main/java/br/com/appvictor/crudcontatos/repositories/PessoaRepository.java
@@ -2,7 +2,11 @@ package br.com.appvictor.crudcontatos.repositories;
 
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import br.com.appvictor.crudcontatos.entities.Pessoa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 @RepositoryRestResource
-public class PessoaRepository {
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
 }
--- src/main/java/br/com/appvictor/crudcontatos/repositories/TelefoneRepository.java
+++ src/main/java/br/com/appvictor/crudcontatos/repositories/TelefoneRepository.java
@@ -1,8 +1,11 @@
 package br.com.appvictor.crudcontatos.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import br.com.appvictor.crudcontatos.entities.Telefone;
+
 @RepositoryRestResource
-public class TelefoneRepository {
+public interface TelefoneRepository extends JpaRepository<Telefone, Long>{
 
 }
